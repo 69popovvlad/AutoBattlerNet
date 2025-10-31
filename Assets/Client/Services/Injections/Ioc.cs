2eb241f770b31731c334b9106d56f29ff5afbbdc
Validosik <69popovvlad@gmail.com>
2025-10-31 22:19:18 +0300

    Implemented static `Ioc` getter

---
+++ Assets/Client/Services/Injections/Ioc.cs
@@ -0,0 +1,9 @@
+﻿using Validosik.Core.Ioc;
+
+namespace Client.Services.Injections
+{
+    public static class Ioc
+    {
+        public static ServiceContainerManager Instance { get; } = new ServiceContainerManager();
+    }
+}
