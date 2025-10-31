1a4e8cc3c993342eadbccb9eb8414c838782e3c4
Validosik <69popovvlad@gmail.com>
2025-11-01 00:27:15 +0300

    Added the `unity.multiplayer.play mode` package for testing multiplayer mode without rebuilding
    Also created a `Network Bootstrapper` for automatically starting the host or connecting based on specified defines

---
+++ Assets/Client/Gameplay/Network/NetworkBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿// #define NET_HOST_TEST
+// #define NET_CLIENT_TEST
+
+using FishNet.Managing;
+using UnityEngine;
+
+namespace Client.Gameplay.Network
+{
+    public class NetworkBootstrapper : MonoBehaviour
+    {
+        [SerializeField] private NetworkManager _networkManager;
+
+        private void Awake()
+        {
+#if NET_HOST_TEST
+            _networkManager.ServerManager.StartConnection();
+            _networkManager.ClientManager.StartConnection();
+#elif NET_CLIENT_TEST
+            _networkManager.ClientManager.StartConnection();
+#endif
+        }
+    }
+}
