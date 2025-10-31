07a5e66ba451f9b542fc030fbb7f41db5051acda
Validosik <69popovvlad@gmail.com>
2025-11-01 00:24:46 +0300

    Added `CharacterContext` for setting movement input, setting camera target and other future features

---
+++ Assets/Client/Gameplay/Network/Character/CharacterContext.cs
@@ -0,0 +1,31 @@
+﻿using Client.Gameplay.Camera;
+using Client.Gameplay.Character.Input;
+using Client.Services.Injections;
+using FishNet.Object;
+using UnityEngine;
+
+namespace Client.Gameplay.Network.Character
+{
+    public class CharacterContext : NetworkBehaviour
+    {
+        [SerializeField]
+        private MovementInputRouter _movementInputRouter;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            if (IsOwner)
+            {
+                _movementInputRouter.Initialize(new KeyboardMovementInput());
+                Ioc.Instance
+                    .Resolve<ICameraTargetProvider>()
+                    .CameraTarget = transform;
+            }
+            else
+            {
+                _movementInputRouter.Initialize(new NoOpMovementInput());
+            }
+        }
+    }
+}
