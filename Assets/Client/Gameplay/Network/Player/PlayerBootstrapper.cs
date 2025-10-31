d5260c33b6a9b0449264acfc26d65273cd370c66
Validosik <69popovvlad@gmail.com>
2025-10-31 22:21:10 +0300

    Added `PlayerBootstrapper` for creating player prefab

---
+++ Assets/Client/Gameplay/Network/Player/PlayerBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Client.Gameplay.Network.Character;
+using FishNet.Object;
+using UnityEngine;
+
+namespace Client.Gameplay.Network.Player
+{
+    public class PlayerBootstrapper : NetworkBehaviour
+    {
+        [SerializeField] private CharacterContext _characterPrefab;
+
+        private CharacterContext _character;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            if (IsOwner)
+            {
+                RequestSpawnCharacterServerRpc();
+            }
+        }
+
+        [ServerRpc]
+        private void RequestSpawnCharacterServerRpc()
+        {
+            var character = Instantiate(_characterPrefab, gameObject.transform);
+            ServerManager.Spawn(character, Owner);
+        }
+    }
+}
