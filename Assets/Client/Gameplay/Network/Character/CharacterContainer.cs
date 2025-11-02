fa225d8461ca975d0651cf7c4202df204e008da9
Validosik <69popovvlad@gmail.com>
2025-11-02 12:57:49 +0300

    Implemented `CharacterContainer` to collect all character in game and find random one for future features

---
+++ Assets/Client/Gameplay/Network/Character/CharacterContainer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Validosik.Core.Ioc.Attributes;
+
+namespace Client.Gameplay.Network.Character
+{
+    [ContainableServiceImplementation("ec9988f6-a8ad-4e17-b6ed-61aeb0f73745", "9982c958-2085-4142-b4ed-7f5b38dcedc8")]
+    public class CharacterContainer : ICharacterContainer
+    {
+        private readonly Dictionary<int, CharacterContext> _characters = new Dictionary<int, CharacterContext>();
+
+        public bool TryGetRandom(out CharacterContext ctx)
+        {
+            if (_characters.Count == 0)
+            {
+                ctx = null;
+                return false;
+            }
+
+            var index = UnityEngine.Random.Range(0, _characters.Count);
+            foreach (var c in _characters.Values)
+            {
+                if (index-- != 0)
+                {
+                    continue;
+                }
+
+                ctx = c;
+                return true;
+            }
+
+            ctx = null;
+            return false;
+        }
+
+        public bool TryAdd(CharacterContext context) =>
+            _characters.TryAdd(context.NetworkObject.ObjectId, context);
+
+        public bool Remove(CharacterContext context) =>
+            _characters.Remove(context.NetworkObject.ObjectId);
+    }
+}
---
+++ Assets/Client/Gameplay/Network/Character/ICharacterContainer.cs
@@ -0,0 +1,14 @@
+﻿using Validosik.Core.Ioc.Attributes;
+
+namespace Client.Gameplay.Network.Character
+{
+    [ContainableServiceContract("ec9988f6-a8ad-4e17-b6ed-61aeb0f73745")]
+    public interface ICharacterContainer
+    {
+        bool TryGetRandom(out CharacterContext ctx);
+
+        bool TryAdd(CharacterContext context);
+
+        bool Remove(CharacterContext context);
+    }
+}
