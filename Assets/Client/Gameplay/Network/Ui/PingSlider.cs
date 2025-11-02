34267d4b7a110a059f52569c724057a45fe38918
Validosik <69popovvlad@gmail.com>
2025-11-03 01:38:22 +0300

    Ping slider

---
+++ Assets/Client/Gameplay/Network/Ui/PingSlider.cs
@@ -0,0 +1,42 @@
+﻿using FishNet.Managing;
+using FishNet.Managing.Transporting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Client.Gameplay.Network.Ui
+{
+    public class PingSlider : MonoBehaviour
+    {
+        [SerializeField] private NetworkManager _networkManager;
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Text _label;
+
+        private TransportManager _tm;
+
+        private void Awake()
+        {
+            _tm = _networkManager.TransportManager;
+
+            var ls = _tm.LatencySimulator;
+            ls.SetEnabled(true);
+
+            _slider.minValue = 0f;
+            _slider.maxValue = 1000f;
+            _slider.wholeNumbers = true;
+            _slider.onValueChanged.AddListener(SetLatency);
+
+            SetLatency(_slider.value);
+        }
+
+        private void SetLatency(float ms)
+        {
+            var ls = _tm.LatencySimulator;
+            ls.SetLatency(Mathf.RoundToInt(ms));
+
+            if (_label)
+            {
+                _label.text = $"{ls.GetLatency()} ms";
+            }
+        }
+    }
+}
