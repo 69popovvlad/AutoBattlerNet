85318bc07684b594e8874af5798a79d9e7c5dc8c
Validosik <69popovvlad@gmail.com>
2025-11-02 22:06:21 +0300

    New `TrackedPool` type for getting references to objects that are not yet in the pool

---
+++ Assets/Client/Services/Pool/IKeyed.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Services.Pool
+{
+    public interface IKeyed<TKey>
+    {
+        TKey Key { get; }
+    }
+}
---
+++ Assets/Client/Services/Pool/TrackedPool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Services.Pool
+{
+    public abstract class TrackedPool<TKey, TValue> : Pool<TValue>
+        where TValue : class, IKeyed<TKey>
+    {
+        private readonly Dictionary<TKey, TValue> _spawned = new Dictionary<TKey, TValue>();
+
+        
+        ///  Don't forget to use Register method
+        public override TValue Rent() => base.Rent();
+        
+        public void Register(TValue value)
+        {
+            _spawned[value.Key] = value;
+        }
+
+        public override void Return(TValue value)
+        {
+            base.Return(value);
+            _spawned.Remove(value.Key);
+        }
+
+        public bool TryGetSpawned(TKey key, out TValue projectile) =>
+            _spawned.TryGetValue(key, out projectile);
+    }
+}
